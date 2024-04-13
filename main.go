cddd7af107f3157e4051824ff01229b10a0b50ef
suchanya <suchanya.n@arise.tech>
2024-04-14 02:46:38 +0700

    add swagger authentication and middleware

--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 package main
 
 import (
+	"log"
+	"os"
+	"time"
+
+	_ "github.com/BubbleCrown/books-crud-project/docs"
 	"github.com/gofiber/fiber/v2"
+	jwtware "github.com/gofiber/jwt/v2"
+	"github.com/gofiber/swagger"
 	"github.com/gofiber/template/html/v2"
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/joho/godotenv"
 )
+
+type User struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+var userAdmin = User{
+	Email:    "admin@example.com",
+	Password: "Password1234",
+}
 
 type Book struct {
 	Id     int    `json:"id"`
@@ -14,13 +33,46 @@ type Book struct {
 // database
 var books []Book
 
+func checkMiddleware(c *fiber.Ctx) error {
+	user := c.Locals("user").(*jwt.Token)
+	claims := user.Claims.(jwt.MapClaims)
+	if claims["role"] != "admin" {
+		return fiber.ErrUnauthorized
+	}
+	return c.Next()
+}
+
+// @title Book API
+// @description This is a sample server for a book API.
+// @version 1.0
+// @host localhost:8080
+// @BasePath /
+// @schemes http
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	engine := html.New("./views", ".html")
 
 	app := fiber.New(fiber.Config{
 		Views: engine,
 	})
+	app.Get("/swagger/*", swagger.HandlerDefault) // default
+
 	books = append(books, Book{Id: 1, Title: "Bubble", Author: "Bubble Crown"})
+
+	app.Post("/login", login)
+
+	app.Use(jwtware.New(jwtware.Config{
+		SigningKey: []byte(os.Getenv("JWT_SECRET")),
+	}))
+
+	app.Use(checkMiddleware)
+
 	app.Get("/books", getAllBooks)
 	app.Get("/books/:id", getBook)
 	app.Post("/books", createBook)
@@ -30,6 +82,7 @@ func main() {
 	app.Post("/upload", uploadFile)
 
 	app.Get("/", renderTemplate)
+	app.Get("/api/config", getConfig)
 	app.Listen(":8080")
 }
 func uploadFile(c *fiber.Ctx) error {
@@ -47,5 +100,46 @@ func uploadFile(c *fiber.Ctx) error {
 func renderTemplate(c *fiber.Ctx) error {
 	return c.Render("index", fiber.Map{
 		"Name": "World",
+	})
+}
+
+func getConfig(c *fiber.Ctx) error {
+	secretKey := os.Getenv("SECRET_KEY")
+	if secretKey == "" {
+		secretKey = "defaultSecret"
+	}
+	return c.JSON(fiber.Map{
+		"secret_key": os.Getenv("SECRET"),
+	})
+}
+
+func login(c *fiber.Ctx) error {
+	user := new(User)
+	if err := c.BodyParser(user); err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	}
+	if user.Email != userAdmin.Email || user.Password != userAdmin.Password {
+		return fiber.ErrUnauthorized
+	}
+
+	// Create the Claims
+	claims := jwt.MapClaims{
+		"email": user.Email,
+		"role":  "admin",
+		"exp":   time.Now().Add(time.Hour * 72).Unix(),
+	}
+
+	// Create token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	// Generate encoded token and send it as response.
+	t, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+	if err != nil {
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+
+	return c.JSON(fiber.Map{
+		"message": "Login successful",
+		"token":   t,
 	})
 }
